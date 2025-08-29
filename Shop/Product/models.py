39cc4e507cebb1a2ec720589e7ccfeebd5f56478
Edward Denis <edorward.21@mail.com>
2025-08-29 15:15:16 +0300

    New but simple

--- Shop/Product/models.py
+++ Shop/Product/models.py
@@ -17,17 +17,18 @@ class Category(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
+        return reverse('Product:product_list_by_category', args=[self.slug])
     
 
 class Product(models.Model):
     category = models.ForeignKey(
         Category,
         related_name='products',
-         on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        null=True
     )
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200, null=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -44,5 +45,5 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+        return reverse('Product:product_detail', args=[self.id, self.slug])
         
