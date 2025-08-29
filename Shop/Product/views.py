e6d97db4aad1e48ceced586d86b6a4a1a80d9dd7
Edward Denis <edorward.21@mail.com>
2025-08-29 14:18:39 +0300

    simple changes

--- Shop/Product/views.py
+++ Shop/Product/views.py
@@ -1,9 +1,8 @@
-
-# Create your views here.
-
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from cart.forms import CartAddProductForm
 
+# Create your views here.
 def product_list(request, category_slug=None):
 	category = None
 	categories = Category.objects.all()
@@ -11,25 +10,14 @@ def product_list(request, category_slug=None):
 	if category_slug:
 		category = get_object_or_404(Category, slug=category_slug)
 		products = products.filter(category=category)
-	return render(
-		request,
-		'shop/product/list.html',
-		{
-			'category': category,
-			'categories': categories,
-			'products': products
-		}
-	)
+	return render(request,'shop/product/list.html',{'category': category,'categories': categories,'products': products})
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(
-        Product, id=id, slug=slug, available=True
-    )
-    return render(
-        request,
-		'shop/product/detail.html',
-        {'product': product}
-    )
+	product = get_object_or_404(
+		Product, id=id, slug=slug, available=True
+	)
+	cart_product_form = CartAddProductForm()
+	return render(request,'shop/product/detail.html',{'product': product, 'cart_product_form': cart_product_form})
 
 def product_search(request):
 	query = request.GET.get('q')
